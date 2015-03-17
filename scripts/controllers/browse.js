3e84db56304012a153af754effdfb29e451bc5cf
JulianoPadilha <sistemas.padilha@gmail.com>
2015-03-17 14:54:48 -0300

    Comment implementation.

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,26 +1,48 @@
 'use strict';
 
-app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment) {
     
     $scope.searchTask = '';
     $scope.tasks = Task.all;
     $scope.signedIn = Auth.signedIn;
     $scope.listMode = true;
 
+    $scope.user = Auth.user;
+
     if($routeParams.taskId) {
         var task = Task.getTask($routeParams.taskId).$asObject();
         $scope.listMode = false; 
-        setSelectedTest(task);
+        setSelectedTask(task);
     }
 
     function setSelectedTask(task) {
-    	$scope.selectedTask = Task;
+    	$scope.selectedTask = task;
 
     	if($scope.signedIn()) {
     		$scope.isTaskCreator = Task.isCreator;
     		$scope.isOpen = Task.isOpen;
     	}
-    }
+
+        $scope.comments = Comment.comments(task.$id);
+    };
+
+    $scope.cancelTask = function(taskId) {
+        Task.cancelTask(taskId).then(function() {
+            toaster.pop('success', "This task is cancelled successfully!");
+        });
+    };
+
+    $scope.addComment = function() {
+        var comment = {
+            content: $scope.content,
+            name: $scope.user.profile.name,
+            gravatar: $scope.user.profile.gravatar
+        };
+
+        Comment.addComment($scope.selectedTask.$id, comment).then(function(){
+            $scope.content = '';
+        });
+    };
 
 
 });
